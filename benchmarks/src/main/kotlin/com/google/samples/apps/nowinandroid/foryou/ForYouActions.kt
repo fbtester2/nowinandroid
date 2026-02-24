2cda1090a2bf5d7a6ddf4c20a2462e4548247daa
Moe <am6691917@gmail.com>
2026-02-25 01:50:06 +0200

    changed the wait time from 10s to 20s for now since it keeps making the runner crash

--- benchmarks/src/main/kotlin/com/google/samples/apps/nowinandroid/foryou/ForYouActions.kt
+++ benchmarks/src/main/kotlin/com/google/samples/apps/nowinandroid/foryou/ForYouActions.kt
@@ -30,7 +30,7 @@ fun MacrobenchmarkScope.forYouWaitForContent() {
     device.wait(Until.gone(By.res("loadingWheel")), 5_000)
     // Sometimes, the loading wheel is gone, but the content is not loaded yet
     // So we'll wait here for topics to be sure
-    val obj = device.waitAndFindObject(By.res("forYou:topicSelection"), 10_000)
+    val obj = device.waitAndFindObject(By.res("forYou:topicSelection"), 20_000)
     // Timeout here is quite big, because sometimes data loading takes a long time!
     obj.wait(untilHasChildren(), 60_000)
 }
